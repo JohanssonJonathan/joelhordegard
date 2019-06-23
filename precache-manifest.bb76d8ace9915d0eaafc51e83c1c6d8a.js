f03542a33a4d92552068a5a239530ef67ea471d6
Jonathan Johansson <jonathanjohansson@Jonathans-MacBook-Air.local>
2019-06-23 15:12:39 +0200

    Updates

---
+++ precache-manifest.bb76d8ace9915d0eaafc51e83c1c6d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61a2274977eeb3e9caf9b6a944bab21",
+    "url": "/joelhordegard/index.html"
+  },
+  {
+    "revision": "4d4d173fb4cf86ad5842",
+    "url": "/joelhordegard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/joelhordegard/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "4d4d173fb4cf86ad5842",
+    "url": "/joelhordegard/static/js/main.2a8e4182.chunk.js"
+  },
+  {
+    "revision": "8ba2058135a5f160e94f",
+    "url": "/joelhordegard/static/js/runtime~main.646685d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joelhordegard/static/media/logo.5d5d9eef.svg"
+  }
+]);
