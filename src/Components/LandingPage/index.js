97329612495a52b9e41d283bee11e35dd3c7c1bb
Jonathan Johansson <jonathanjohansson@Jonathans-MacBook-Air.local>
2019-10-04 21:08:21 +0200

    implement landingpage component with image

---
+++ src/Components/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../../CSS/LandingPage.css"
+
+
+const LandingPage =()=>{
+    return (
+        <div className="container-landing-page"  style={{ backgroundImage: `url("http://fotoklubben.fi/wp-content/uploads/2019/01/Deltagare23_IntotheVoid-e1548690409286.jpg")` }}>
+            {/* <img src="http://fotoklubben.fi/wp-content/uploads/2019/01/Deltagare23_IntotheVoid-e1548690409286.jpg"/> */}
+        </div>
+    )
+}
+
+export default LandingPage
