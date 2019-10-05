57074a9fb6517195b6695c8213425fe9b22b3f9b
Jonathan Johansson <jonathanjohansson@Jonathans-MacBook-Air.local>
2019-10-05 20:04:29 +0200

    Updates

---
+++ precache-manifest.a895722805ae68798c431f50be1e0f93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9dc2c00f5dcb9ccf6aba3568eac1b1",
+    "url": "/joelhordegard/index.html"
+  },
+  {
+    "revision": "5c3bf5a29d010970147f",
+    "url": "/joelhordegard/static/css/main.355a8879.chunk.css"
+  },
+  {
+    "revision": "52fcfac4f9b4fc8086d4",
+    "url": "/joelhordegard/static/js/2.39769edb.chunk.js"
+  },
+  {
+    "revision": "5c3bf5a29d010970147f",
+    "url": "/joelhordegard/static/js/main.b694d22c.chunk.js"
+  },
+  {
+    "revision": "8ba2058135a5f160e94f",
+    "url": "/joelhordegard/static/js/runtime~main.646685d2.js"
+  },
+  {
+    "revision": "084ac2e3fdfc2ba3a42e3fea7af81d77",
+    "url": "/joelhordegard/static/media/Helvetica.084ac2e3.ttf"
+  },
+  {
+    "revision": "7395c5f89f8db5ad5138690629046dac",
+    "url": "/joelhordegard/static/media/Times-SemiBold.7395c5f8.ttf"
+  },
+  {
+    "revision": "8f28278542f33d6327a9bf137ee8d07b",
+    "url": "/joelhordegard/static/media/backgroundVideo.8f282785.mp4"
+  }
+]);
