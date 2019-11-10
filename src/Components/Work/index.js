38331819501ec560c8bd2ad339748f349906e9a9
Jonathan Johansson <jonathanjohansson@Jonathans-MacBook-Air.local>
2019-11-10 13:04:53 +0100

    remove dummydata

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -2,21 +2,16 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Videos from "./Videos";
 
-const Home = (props) =>
-  props.media.length > 0 && props.posts.length > 0 ? (
+const Home = (props) => {
+  return props.posts.length > 0 ? (
     <div className={"container"}>
       <Header />
-
       <Videos media={props.media} title={props.posts} />
     </div>
   ) : (
-    <div className={"container"}>
-      <Header />
-      <Videos
-        media={props.dummyDataMedia}
-        title={props.dummyDataPosts}
-      />
-    </div>
+null
   );
+}
+ 
 
 export default Home;
